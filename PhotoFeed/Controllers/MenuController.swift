f491f3b56879873231a5fe8f5bf0dc38ba481277
Khoa Pham <onmyway133@gmail.com>
2017-08-10 21:38:28 +0200

    Add MenuController

--- PhotoFeed/Controllers/MenuController.swift
+++ PhotoFeed/Controllers/MenuController.swift
@@ -1,5 +1,22 @@
 import UIKit
 
-class MenuController: UIViewController {
-  
+protocol MenuControllerDelegate: class {
+  func menuControllerDidSelectLogout(_ controller: MenuController)
+}
+
+class MenuController: UITableViewController {
+  enum Section: Int {
+    case about
+    case account
+  }
+
+  weak var delegate: MenuControllerDelegate?
+
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+
+    if indexPath.section == Section.account.rawValue, indexPath.row == 0 {
+      delegate?.menuControllerDidSelectLogout(self)
+    }
+  }
 }
