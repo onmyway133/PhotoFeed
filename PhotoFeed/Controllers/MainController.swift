0eba725f758bb5a49d11843a2b99e642656530e6
Khoa Pham <onmyway133@gmail.com>
2017-09-07 19:49:12 +0200

    Register for remote notifications

--- PhotoFeed/Controllers/MainController.swift
+++ PhotoFeed/Controllers/MainController.swift
@@ -29,7 +29,19 @@
  */
 
 import UIKit
+import UserNotifications
 
 class MainController: UITabBarController {
-  
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Register to get device token for remote notifications
+    UIApplication.shared.registerForRemoteNotifications()
+    // Register to handle push notification UI
+    let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+    UNUserNotificationCenter.current().requestAuthorization(options: options) { (granted, error) in
+      print(error as Any)
+    }
+  }
 }
